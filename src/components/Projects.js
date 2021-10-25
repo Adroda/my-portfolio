c74133ec411f12cbd79fcbbb8a4d0ddb1472de45
adroda <adrianchi_91@hotmail.com>
2021-10-25 18:57:00 -0300

    About

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,14 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
 const Projects = () => {
-  return null;
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full- mb-20"></div>
+      </div>
+    </section>
+  );
 };
 export default Projects;
